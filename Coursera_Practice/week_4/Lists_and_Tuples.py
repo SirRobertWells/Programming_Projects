536e1b640a0bd84761bd26d3dd20c4c09fa9262d
SirRobertWells <60278791+SirRobertWells@users.noreply.github.com>
2020-08-27 12:28:36 -0500

    solved problem

---
+++ Coursera_Practice/week_4/Lists_and_Tuples.py
@@ -0,0 +1,29 @@
+# Tuples - Sequences of elements of any type,
+# that are immutable
+
+fullname = ('Grace', 'M', 'Hopper')
+# the position of the elements inside the tuple have meaning
+
+
+#original problem
+
+#def file_size(file_info):
+#	___, ___, ___= file_info
+#	return("{:.2f}".format(___ / 1024))
+
+#print(file_size(('Class Assignment', 'docx', 17875))) # Should print 17.46
+#print(file_size(('Notes', 'txt', 496))) # Should print 0.48
+#print(file_size(('Program', 'py', 1239))) # Should print 1.21
+###
+###
+
+
+#solved
+def file_size(file_info):
+    (name, type, size) = file_info
+    return ("{:.2f}".format(size / 1024))
+
+
+print(file_size(('Class Assignment', 'docx', 17875)))  # Should print 17.46
+print(file_size(('Notes', 'txt', 496)))  # Should print 0.48
+print(file_size(('Program', 'py', 1239)))  # Should print 1.21
