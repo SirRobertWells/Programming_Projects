e2caedf5ed898dbbf21fd35d9f656a907ec4c15a
SirRobertWells <60278791+SirRobertWells@users.noreply.github.com>
2020-08-27 15:01:35 -0500

    setting up files for week 4

--- Coursera_Practice/week_4/iterating_over_lists_and_tuples.py
+++ Coursera_Practice/week_4/iterating_over_lists_and_tuples.py
@@ -6,9 +6,11 @@ for animal in animals:
 print('Total characters: {}, Average length: {}'.format(chars, chars / len(animals)))
 
 # now going over the "enumerate" function
-winners = ['Ashley','Dylan', 'Reese']
+winners = ['Ashley', 'Dylan', 'Reese']
 for index, each_person in enumerate(winners):
     print('{} - {}'.format(index + 1, each_person))
+
+
 # The enumerate function returns a tuple for each element (each_person) in the list (winners).
 # The first value {} in the tuple is the index (index + 1) of the element in the sequence.
 # The second value {} in the tuple is the element (each_person) in the sequence.
@@ -19,22 +21,36 @@ for index, each_person in enumerate(winners):
 # Complete the skip_elements function to return every other element from the list,
 # this time using the enumerate function to check if
 # an element is on an even position or an odd position.
-#def skip_elements(elements):
+# def skip_elements(elements):
 #    # code goes here
 #
 #   return ___
 #
 #
-#print(skip_elements(["a", "b", "c", "d", "e", "f", "g"]))  # Should be ['a', 'c', 'e', 'g']
-#print(skip_elements(
+# print(skip_elements(["a", "b", "c", "d", "e", "f", "g"]))  # Should be ['a', 'c', 'e', 'g']
+# print(skip_elements(
 #    ['Orange', 'Pineapple', 'Strawberry', 'Kiwi', 'Peach']))  # Should be ['Orange', 'Strawberry', 'Peach']
 def skip_elements(elements):
     # code goes here
-    for i
-
-    return ___
+    new_list = []
+    i = 0
+    for i in range(len(elements)):
+        if i % 2 == 0:
+            new_list.append(elements[i])
+            i += 1
+    return new_list
 
 
 print(skip_elements(["a", "b", "c", "d", "e", "f", "g"]))  # Should be ['a', 'c', 'e', 'g']
 print(skip_elements(
     ['Orange', 'Pineapple', 'Strawberry', 'Kiwi', 'Peach']))  # Should be ['Orange', 'Strawberry', 'Peach']
+
+
+def full_emails(people):
+    result = []
+    for email, name in people:
+        result.append('{} <{}>'.format(name, email))
+    return result
+
+
+print(full_emails([('alex@example.com', 'Alex Diego'), ('shay@example.com', 'Shay Brandt')]))
