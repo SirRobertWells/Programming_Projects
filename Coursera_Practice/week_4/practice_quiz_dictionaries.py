ca02d50db459b27857cc246436f662bdbfcce989
SirRobertWells <60278791+SirRobertWells@users.noreply.github.com>
2020-08-27 23:27:45 -0500

    updated folder for freeCodeCamp

---
+++ Coursera_Practice/week_4/practice_quiz_dictionaries.py
@@ -0,0 +1,9 @@
+def email_list(domains):
+	emails = []
+	for ___:
+	  for user in users:
+	    emails.___
+	return(emails)
+
+print(email_list({"gmail.com": ["clark.kent", "diana.prince", "peter.parker"],
+                  "yahoo.com": ["barbara.gordon", "jean.grey"], "hotmail.com": ["bruce.wayne"]}))
